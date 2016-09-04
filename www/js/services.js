3976d8348cf4f2a64c5c9d6c6e56b15bd3f87136
Renato Violin <renatoviolin@gmail.com>
2016-09-04 14:57:47 -0300

    Atualização no HOST do banco de dados

--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,9 @@
 angular.module('starter.services', ['ngResource'])
 
 // .constant('baseURL', 'http://localhost:8000/')
-.constant('baseURL', 'http://192.168.1.48:8000/')
+// .constant('baseURL', 'http://192.168.1.48:8000/')
+.constant('baseURL', 'https://nodedb-partitura.herokuapp.com/')
+
 
 .factory('httpPartitura', function($resource, baseURL) {
    return $resource(baseURL + 'partitura/:id');
